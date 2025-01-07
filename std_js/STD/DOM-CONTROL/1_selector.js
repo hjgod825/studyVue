22ad811ec723afd4b280a615f5dc0cae3d461558
x3249444@gmail.com <x3249444@gmail.com>
2025-01-07 17:47:14 +0900

    3장준비완료

--- std_js/STD/DOM-CONTROL/1_selector.js
+++ std_js/STD/DOM-CONTROL/1_selector.js
@@ -1,5 +1,18 @@
-let h2 = document.querySelector('#title');
-console.log('h2 : ', h2);
+// let h2 = document.querySelector('#title');
+// console.log('h2 : ', h2);
 
-h2.textContent = '운동!!!';
-h2.innerHTML = '<span>운동@@@</span>';
+// // h2.textContent = '운동!!!';
+// // h2.innerHTML = '<span>운동@@@</span>';
+
+// // console.log(h2.textContent);
+
+// let input = document.querySelector('input');
+
+// input.setAttribute('placeholder', '운동을 저장하여 주십시오');
+// input.setAttribute('required', '');
+// input.removeAttribute('placeholder');
+
+let helloitem = document.querySelector('.hello');
+// console.log(helloitem);
+// helloitem.style.color = 'white';
+// helloitem.style.backgroundColor = 'black';
